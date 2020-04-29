2dd566a1f2bea7e06eb528c31919a5664afc37ce
NPospolita <nikita7asics@gmail.com>
2020-04-29 14:36:39 +0300

    Better uptime formatting

--- src/main/java/com/rbkmoney/actuator/indicators/ServiceVersionHealthIndicator.java
+++ src/main/java/com/rbkmoney/actuator/indicators/ServiceVersionHealthIndicator.java
@@ -9,6 +9,7 @@ import org.springframework.boot.actuate.info.InfoEndpoint;
 import javax.annotation.PostConstruct;
 import java.lang.management.ManagementFactory;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,12 +21,29 @@ public class ServiceVersionHealthIndicator implements HealthIndicator {
     public Health health() {
         return Health.up()
                 .withDetail("git.commit", ((Map<String, Object>)((Map<String, Object>)infoEndpoint.info().get("git")).get("commit")).get("id"))
-                .withDetail("uptime", ManagementFactory.getRuntimeMXBean().getUptime() / 1000 + "sec")
+                .withDetail("uptime", getUptimeFormatted(ManagementFactory.getRuntimeMXBean().getUptime()))
                 .build();
     }
 
     @PostConstruct
     public void onStart() {
         log.info("Added service version health indicator");
+    }
+
+
+    protected String getUptimeFormatted(long uptimeMillis) {
+
+        long days = TimeUnit.MILLISECONDS.toDays(uptimeMillis);
+        uptimeMillis -= TimeUnit.DAYS.toMillis(days);
+
+        long hours = TimeUnit.MILLISECONDS.toHours(uptimeMillis);
+        uptimeMillis -= TimeUnit.HOURS.toMillis(hours);
+
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(uptimeMillis);
+        uptimeMillis -= TimeUnit.MINUTES.toMillis(minutes);
+
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(uptimeMillis);
+
+        return String.format("%dd, %dh, %dm, %ds ", days, hours, minutes, seconds);
     }
 }
